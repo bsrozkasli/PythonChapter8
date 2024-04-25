0cfd10c80cce2b019284467dfe019591249e3ce7
Basar Ozkasli <basarozkasli@gmail.com>
2024-04-25 15:46:45 +0300

    added practice about timer functions

--- chapter8.4.py
+++ chapter8.4.py
@@ -4,3 +4,15 @@ time_here=localtime()
 print(f"Year:{time_here[0]},{time_here.tm_year}")
 print(f"Month:{time_here[1]},{time_here.tm_mon}")
 print(f"Year:{time_here[2]},{time_here.tm_mday}")
+import time
+from time import time as my_timer
+import random as rnd
+input("Press enter to start")
+wait_time=rnd.randint(1,6)
+time.sleep(wait_time)
+start_time=my_timer()
+input("Press enter to stop")
+end_time=my_timer()
+print("Started at"+time.strftime("%X",time.localtime(start_time)))
+print("Ended at "+time.strftime("%X",time.localtime(end_time)))
+print("Your reaction time was {} seconds".format(end_time-start_time))
