535bea4fb7873042465566f9bf54eba7f3c1f7a6
Basar Ozkasli <basarozkasli@gmail.com>
2024-04-25 15:53:41 +0300

    added chapter mutable,8.1,8.2,8.3

---
+++ chapter8.3.py
@@ -0,0 +1,3 @@
+import webbrowser
+#  
+print(dir(webbrowser))
---
+++ mutable.py
@@ -0,0 +1,12 @@
+import time
+done='Well deserved for your new job'
+from turtle import forward,right,left,done
+import turtle
+
+forward(20)
+right(150)
+forward(20)
+turtle.circle(75)
+done()
+print(done)
+# time.sleep(5)
